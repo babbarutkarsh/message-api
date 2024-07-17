e3feaaeb77cb04babfaa584d778f3c217df25353
babbarutkarsh <46282290+babbarutkarsh@users.noreply.github.com>
2024-07-17 11:05:17 +0530

    Update security-groups.tf

--- terraform-module/security-groups.tf
+++ terraform-module/security-groups.tf
@@ -2,9 +2,6 @@ resource "aws_security_group" "kubernetes_nodes" {
   name        = "kubernetes-nodes-sg"
   description = "Security group for Kubernetes nodes"
   vpc_id      = aws_vpc.main.id
-
-  # Define ingress and egress rules as needed
-  # Example: allow SSH and required ports for Kubernetes nodes
 }
 
 resource "aws_security_group" "kubernetes_control_plane" {
